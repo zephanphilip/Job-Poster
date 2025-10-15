b2a699419faa4175eee711d3994a2337fca5d0e0
Zephan Philip <zephanphilip03@gmail.com>
2025-10-15 13:30:51 +0530

    frontend origin updated

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,8 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
     app.enableCors({
-    origin: 'https://jobposter-ivory.vercel.app/jobs', 
+    origin: ['https://jobposter-ivory.vercel.app','http://localhost:3000',               // for local testing
+  ], 
     methods: ['GET','POST','PUT','DELETE','OPTIONS'],
     credentials: true,
   });
